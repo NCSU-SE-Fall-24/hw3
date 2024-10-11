dd084aeb1479cef5e6c7a21285c8ec243c04aef8
Kenil2211 <kenilpatel221102@gmail.com>
2024-10-10 21:10:14 -0400

    fix(task2): :bug: Task2 one-liner

--- task2.sh
+++ task2.sh
@@ -1,41 +1,7 @@
-#!/bin/bash
-printf "%-30s %-10s %-10s\n" "Name" "Size" "Occurrence"
-grep -rl "sample" dataset1 | while read line; do
-    count=$(cat $line | tr ' ' '\n' | grep "CSC510" | grep -c "CSC510" | uniq -c | awk '{print $2}')
-    if [ $count -ge 3 ]; then
-        file=$(echo $line)
-        size=$(stat -f%z "$file")
-        filtered_file=${file/file_/filtered_}
-        # Output the file details
-        printf "%-30s %-10d %-10d\n" "$filtered_file" "$size" "$count"
+grep -l "sample" dataset1/* | while read -r file; do
+    count=$(grep -o "CSC510" "$file" | wc -l)
+    if (( count >= 3 )); then
+        size=$(wc -c < "$file")
+        echo "$count $size $file"
     fi
-done | gawk '{
-    print $0
-}' | gawk 'BEGIN { OFS = "\n" }
-{
-    data[NR] = $0
-    split($0, fields, " ")
-    sizes[NR] = fields[2]
-    occurrences[NR] = fields[3]
-}
-END {
-    for (i = 1; i <= NR; i++) {
-        for (j = i + 1; j <= NR; j++) {
-            if (occurrences[i] < occurrences[j] || (occurrences[i] == occurrences[j] && sizes[i] < sizes[j])) {
-                # Swap the lines
-                temp = data[i]
-                data[i] = data[j]
-                data[j] = temp
-                tempSize = sizes[i]
-                sizes[i] = sizes[j]
-                sizes[j] = tempSize
-                tempOcc = occurrences[i]
-                occurrences[i] = occurrences[j]
-                occurrences[j] = tempOcc
-            }
-        }
-    }
-    for (i = 1; i <= NR; i++) {
-        print data[i]
-    }
-}'
+done | sort -nr | sort -k1,1nr -k2,2n | awk '{print $3}' | sed 's/file_/filtered_/g'
