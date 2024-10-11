46395d7e76614ed24cd3af7c689617a016c84ceb
Kenil2211 <kenilpatel221102@gmail.com>
2024-10-10 21:27:21 -0400

    fix-task2 script

--- task2.sh
+++ task2.sh
@@ -1,10 +1,10 @@
-grep -l "sample" dataset1/* | 
+grep -l "sample" dataset1/* | \
 xargs -I{} sh -c '
-  count=$(grep -o "CSC510" "{}" | wc -l); 
-  if [ "$count" -ge 3 ]; then 
-    size=$(wc -c < "{}"); 
-    echo "$count $size {}"; 
-  fi' | 
-sort -k1,1nr -k2,2nr |  # Sort by occurrences (desc) and size (desc)
-awk '{print $1, $2, $3}' | 
-sed 's/file_/filtered_/g'
+count=$(grep -o "CSC510" "{}" | wc -l);
+if [ "$count" -ge 3 ]; then
+    size=$(wc -c < "{}");
+    echo "$count $size {}";
+fi' | \
+sort -k1,1nr -k2,2nr |  # Step 2: Sort by occurrences (descending) and size (descending)
+gawk '{print $3}' | \
+sed 's/file_/filtered_/g'  # Step 3: Substitute "file_" with "filtered_"
