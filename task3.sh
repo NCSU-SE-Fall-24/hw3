b34f5d17f6996e963445e32582d84beeba8f0318
Jap Purohit <pjap041@gmail.com>
2024-10-21 22:01:56 -0400

    updated the task3.sh

--- task3.sh
+++ task3.sh
@@ -1,6 +1,5 @@
-input_file="titanic.csv"
-
-cat "$input_file" |
-    awk -F"," '$3 == "2" && $13 == "S" && $7 != "" {print $0}' |
-    sed -e 's/,male,/,M,/g' -e 's/,female,/,F,/g' |
-    tee >(awk -F"," '{total_age += $7; count++} END {if (count > 0) print "Average Age: " total_age / count; else print "No passengers found."}')
+#!/bin/bash
+initial_filter=$(gawk -F, 'NR >= 2 && $3 == 2 && $13 ~ /S/ {print $0}' titanic.csv)
+replacing_gender=$(echo "$initial_filter" | sed -e 's/,male,/,M,/' -e 's/,female,/,F,/')
+final_output=$(echo "$replacing_gender" |  gawk -F, 'BEGIN {count = 0; sum_age = 0} {if ($7 != "") {count+=1; sum_age+=$7; print $0}} END {if (count > 0) print "Average age: " sum_age/count; else print "No valid data"}')
+echo "$final_output"
